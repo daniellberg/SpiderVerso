c0b7302be527e210a45c37f263ff616cd604d4bf
daniellberg <danielbergons@gmail.com>
2023-11-26 16:32:22 -0300

    heroes data req and creates components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1>Hello, world!</h1>;
+import styles from "./page.module.scss";
+
+import HeroesList from "@/components/index";
+import { IHeroData } from "@/interfaces/heroes";
+
+const getHeroesData = async (): Promise<{ data: IHeroData[] }> => {
+  const res = await fetch(`${process.env.DOMAIN_ORIGIN}/api/heroes`);
+
+  if (!res.ok) {
+    throw new Error("Failed to request heroes list");
+  }
+
+  return res.json();
+};
+
+export default async function Home() {
+  const heroes = await getHeroesData();
+
+  return (
+    <main className={styles.main}>
+      <HeroesList heroes={heroes.data} />
+    </main>
+  );
 }
